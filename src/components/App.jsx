db7aec05f83c1e585834eb097af27ecdf67b594a
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-10-31 13:43:41 +0200

    test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* test comment */}
+      React homework template some change for test
     </div>
   );
 };
